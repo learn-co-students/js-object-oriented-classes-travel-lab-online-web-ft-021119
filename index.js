3584297edeaaebc5fc012448084dc142dae483a6
Lizzie <lchan217@gmail.com>
2019-05-25 18:16:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+class Driver {
+  constructor(name, startDate){
+    this.name = name
+    this.startDate = new Date(startDate)
+  }
+  yearsExperienceFromBeginningOf(year){
 
+    return Math.abs(year - this.startDate.getFullYear())
+  }
+}
+
+let eastWest = [
+'1st Avenue',
+'2nd Avenue',
+'3rd Avenue',
+'Lexington Avenue',
+'Park',
+'Madison Avenue',
+'5th Avenue'
+];
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled(){
+    let horizontalDist = (eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal))
+    let verticalDist = this.endingLocation.vertical - this.beginningLocation.vertical
+    return verticalDist + horizontalDist
+  }
+
+  estimatedTime(peak ){
+    if (peak === true){
+      return this.blocksTravelled() / 2
+    } else {
+      return this.blocksTravelled() / 3
+    }
+  }
+}
