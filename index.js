1d9ae4c6664fd2d56b89ca5edc358116492f0be9
github username <github email address>
2019-05-27 17:31:14 -0400

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,45 @@ class Driver{
     return end - start + 1;
   }
 }
+
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
+
+class Route {
+  constructor(begin, ending) {
+    this.beginningLocation = begin
+    this.endingLocation = ending
+  }
+  blocksTravelled() {
+
+    let horizontalStart = eastWest.indexOf(this.beginningLocation.horizontal)
+    let horizontalEnd = eastWest.indexOf(this.endingLocation.horizontal)
+    let horizontalBlocksTraveled = Math.abs(horizontalStart - horizontalEnd)
+
+    let verticalBlocksTraveled = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+
+    return horizontalBlocksTraveled + verticalBlocksTraveled
+  }
+
+  estimatedTime(peak) {
+    if (peak) {
+      return this.blocksTravelled() / 2;
+    } else {
+      return this.blocksTravelled() / 3;
+    }
+  }
+
+}
+
+//   // peak = 2bm == 6 min for 12 blocks
+//   // off peak = 3bpm == 4 min for 12 blocks
+//
+//   // peak => this.blocksTravelled() / 2
+//   // off peak => this.blocksTravelled() / 3
