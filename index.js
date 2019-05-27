3128d0fa19af76aa37b63a1e32eaa75958f55e7f
github username <github email address>
2019-05-27 16:23:05 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+class Driver{
+  constructor(name, startDate)
+  {
+    this.name = name
+    this.startDate = new Date(startDate) 
+  }
+  yearsExperienceFromBeginningOf(endDate) {
+    let end = new Date(endDate).getFullYear()
+    let start = this.startDate.getFullYear()
 
+    return end - start + 1;
+  }
+}
