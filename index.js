e5f2ccf66150d38e88cd9605ece2b6e79fa3bbc3
bbennett7 <bryn.bennett@gmail.com>
2019-05-27 01:32:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+class Driver {
+  constructor(name, dayJoined) {
+    this.name = name;
+    this.startDate = new Date(dayJoined);
+  }
 
+  yearsExperienceFromBeginningOf() {
+    let currentDate = new Date();
+    return currentDate.getFullYear() - this.startDate.getFullYear() ;
+  }
+}
+
+
+let eastWest = [
+'1st Avenue',
+'2nd Avenue',
+'3rd Avenue',
+'Lexington Avenue',
+'Park',
+'Madison Avenue',
+'5th Avenue'
+];
+
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  };
+
+  blocksTravelled() {
+    let horizontalBlocks =  Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal));
+
+    let verticalBlocks = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical);
+
+    return horizontalBlocks + verticalBlocks;
+  };
+
+  estimatedTime(peakHours) {
+    if (peakHours === true) {
+      return this.blocksTravelled()/2;
+    } else {
+      return this.blocksTravelled()/3;
+    };
+  };
+
+}
