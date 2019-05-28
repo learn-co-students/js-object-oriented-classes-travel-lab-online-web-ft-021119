9720626aaf63ac816a5345fc5a850244ecc387a1
Christoph Gruener <christophgruener@me.com>
2019-05-28 18:44:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+class Driver {
+  constructor (name, joined) {
+    this.name = name;
+    this.joined = joined;
+  }
 
+  startDate() {
+    return new Date(this.joined)
+  }
+
+  yearsExperienceFromBeginningOf(endDate) {
+    return new Date (endDate) - new Date(this.joined)
+  }
+}
